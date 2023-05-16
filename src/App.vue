67c424b4621c0bac91f9b863f33eaa5943019f82
AntonKurenov <anton.kurenov@yandex.ru>
2023-05-17 01:52:43 +0300

    add todo functionality addedd

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,82 @@
 <template>
-  <div>
-    Hello world!!
+  <div class="app">
+    <header>Todo List</header>
+    <div class="input-form" @submit.prevent>
+      <form @submit.>
+        <input
+            v-bind:value="text"
+            @input="text = $event.target.value"
+            type="text"
+            class="form-input"
+            placeholder="Input todo here..."
+        />
+        <button class="button-add" @click="addTodo">Add Todo</button>
+      </form>
+    </div>
+    <div class="tasks" v-for="task in tasks">
+      <div>{{ task.text }}</div>
+    </div>
   </div>
-  <div>
-    <button @click="addPlus"> +</button>
-    <button @click="addMinus"> -</button>
-  </div>
-  <div>Plus {{ pluses }}</div>
-  <div>Minus{{ minuses }}</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      pluses: 0,
-      minuses: 0,
+      tasks: [
+        {id: 1, text: 'Hello world!'},
+        {id: 2, text: 'Learn Vue!'},
+      ],
+      text: '',
     }
   },
   methods: {
-    addPlus() {
-      this.pluses += 1;
+    addTodo() {
+
+      const newTodo = {
+        id: this.tasks.length,
+        text: this.text,
+      }
+      this.tasks.push(newTodo);
+      this.text = '';
     },
-    addMinus() {
-      this.minuses += 1;
-    }
   }
 }
 
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.app {
+  padding: 10px;
+}
+
+.input-form {
+  padding: 15px;
+}
+
+form {
+  padding: 10px;
+}
+
+.tasks {
+  margin: 10px;
+  padding: 10px;
+  border: aqua 2px solid;
+}
+
+.form-input {
+  height: 100%;
+  border: aqua 2px solid;
+}
+
+.button-add {
+  height: 100%;
+  margin-left: 20px;
+}
 </style>
